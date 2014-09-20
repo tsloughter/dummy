0bee084db06322c23b480a4b1f44f9c7548b5649
Tristan Sloughter <t@crashfast.com>
2014-09-20 08:44:05 -0500

    fix compile error

--- src/dummy.erl
+++ src/dummy.erl
@@ -14,14 +14,14 @@
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    State1 = rebar_state:(State, rebar_provider:create([{name, ?PROVIDER},
-                                                        {provider_impl, ?MODULE},
-                                                        {bare, false},
-                                                        {deps, ?DEPS},
-                                                        {example, "rebar dummy"},
-                                                        {short_desc, "dummy plugin."},
-                                                        {desc, ""},
-                                                        {opts, []}])),
+    State1 = rebar_state:add_provider(State, rebar_provider:create([{name, ?PROVIDER},
+                                                                    {provider_impl, ?MODULE},
+                                                                    {bare, false},
+                                                                    {deps, ?DEPS},
+                                                                    {example, "rebar dummy"},
+                                                                    {short_desc, "dummy plugin."},
+                                                                    {desc, ""},
+                                                                    {opts, []}])),
     {ok, State1}.
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()}.
