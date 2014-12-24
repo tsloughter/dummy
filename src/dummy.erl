d3dc6050be34ced29798f416ee4dee0c3c8425b8
Tristan Sloughter <t@crashfast.com>
2014-12-24 10:42:31 -0600

    update for latest version of providers and rebar3

--- src/dummy.erl
+++ src/dummy.erl
@@ -1,9 +1,10 @@
 -module(dummy).
 
--behaviour(rebar_provider).
+-behaviour(provider).
 
 -export([init/1,
-         do/1]).
+         do/1,
+         format_error/1]).
 
 -define(PROVIDER, dummy).
 -define(DEPS, []).
@@ -14,17 +15,21 @@
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    State1 = rebar_state:add_provider(State, rebar_provider:create([{name, ?PROVIDER},
-                                                                    {provider_impl, ?MODULE},
-                                                                    {bare, false},
-                                                                    {deps, ?DEPS},
-                                                                    {example, "rebar dummy"},
-                                                                    {short_desc, "dummy plugin."},
-                                                                    {desc, ""},
-                                                                    {opts, []}])),
+    State1 = rebar_state:add_provider(State, providers:create([{name, ?PROVIDER},
+                                                               {provider_impl, ?MODULE},
+                                                               {bare, false},
+                                                               {deps, ?DEPS},
+                                                               {example, "rebar dummy"},
+                                                               {short_desc, "dummy plugin."},
+                                                               {desc, ""},
+                                                               {opts, []}])),
     {ok, State1}.
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()}.
 do(State) ->
     io:format("I am a dummy plugin~n"),
     {ok, State}.
+
+-spec format_error(any()) -> iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
